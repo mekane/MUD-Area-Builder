46a9d964c98120a8c987603c740e84655cda0b60
Marty Kane <marty@pixotech.com>
2017-11-24 14:32:41 -0600

    Hooked up the coordinates to the room mapper, displaying them as text

--- src/components/rooms/RoomMap.js
+++ src/components/rooms/RoomMap.js
@@ -1,5 +1,6 @@
 const React = require('react'); //because the JSX transpiles to React.createElement(...) we need this here implicitly
 const vnumLogic = require('../../logic/vnums.js');
+const mapper = require('../../logic/roomMapper.js');
 
 const RoomMap = ({areaInfo, roomsData}) => {
     const vnum = vnumLogic.generator(areaInfo.minVnum);
@@ -7,13 +8,13 @@ const RoomMap = ({areaInfo, roomsData}) => {
     const Room = (room) => (
         <div key={room.id} className="room-map__room">
             <div className="room-map__room-name">#{ vnum(room.id) } { room.name }</div>
+            <div className="room-map__room-coordinates">{ room.coordinates.x }, { room.coordinates.y }</div>
         </div>
     );
 
-    const room = roomsData.byId;
-
-    const allRooms = Object.keys(roomsData.byId).map(roomId => roomsData.byId[roomId]);
-
+    const roomsMapped = mapper.generateCoordinates(roomsData.byId);
+console.log('map', roomsMapped);
+    const allRooms = Object.keys(roomsMapped).map(roomId => roomsMapped[roomId]);
 
     return (
         <div className="room-map">
