745acc794a902b5307e67abecbec1f7a205fa919
Marty Kane <marty@pixotech.com>
2017-11-24 16:08:49 -0600

    Add some styles to show rooms as boxes on the map

--- src/components/rooms/RoomMap.js
+++ src/components/rooms/RoomMap.js
@@ -2,18 +2,32 @@ const React = require('react'); //because the JSX transpiles to React.createElem
 const vnumLogic = require('../../logic/vnums.js');
 const mapper = require('../../logic/roomMapper.js');
 
+const roomSize = 80;
+const gapBetweenRoom = 40;
+
+const cssValueForCoordinate = (val) => val * (roomSize + gapBetweenRoom);
+
 const RoomMap = ({areaInfo, roomsData}) => {
     const vnum = vnumLogic.generator(areaInfo.minVnum);
 
-    const Room = (room) => (
-        <div key={room.id} className="room-map__room">
-            <div className="room-map__room-name">#{ vnum(room.id) } { room.name }</div>
-            <div className="room-map__room-coordinates">{ room.coordinates.x }, { room.coordinates.y }</div>
-        </div>
-    );
+    const Room = (room) => {
+        const x = cssValueForCoordinate(room.coordinates.x);
+        const y = cssValueForCoordinate(room.coordinates.y);
+
+        const style = {
+            transform: `translate(${x}px, ${y}px)`
+        };
+
+        return (
+            <div key={room.id} className="room-map__room" style={style}>
+                <div className="room-map__room-name">#{ vnum(room.id) } { room.name }</div>
+                <div className="room-map__room-coordinates">{ room.coordinates.x }, { room.coordinates.y }</div>
+            </div>
+        );
+    };
 
     const roomsMapped = mapper.generateCoordinates(roomsData.byId);
-console.log('map', roomsMapped);
+
     const allRooms = Object.keys(roomsMapped).map(roomId => roomsMapped[roomId]);
 
     return (
