e40c6a0e39664c6ebd3a0539ffff00e256be85e3
Marty Kane <marty@pixotech.com>
2017-11-24 21:05:22 -0600

    Add elements for actual exits and buttons to add rooms

--- src/components/rooms/RoomMap.js
+++ src/components/rooms/RoomMap.js
@@ -3,9 +3,21 @@ const vnumLogic = require('../../logic/vnums.js');
 const mapper = require('../../logic/roomMapper.js');
 
 const roomSize = 80;
-const gapBetweenRoom = 40;
+const gapBetweenRooms = 40;
 
-const cssValueForCoordinate = (val) => val * (roomSize + gapBetweenRoom);
+const exits = ['n', 'e', 's', 'w'];
+
+const cssValueForCoordinate = (val) => val * (roomSize + gapBetweenRooms);
+
+const Exit = (room, direction) => {
+    const exit = room.exit[direction];
+    if (exit && exit.destination) {
+        return <div className={`room-map__room-exit--${direction}`}></div>;
+    }
+    else {
+        return <div className={`room-map__room-exit-add--${direction}`}></div>;
+    }
+};
 
 const RoomMap = ({areaInfo, roomsData}) => {
     const vnum = vnumLogic.generator(areaInfo.minVnum);
@@ -22,8 +34,19 @@ const RoomMap = ({areaInfo, roomsData}) => {
             <div key={room.id} className="room-map__room" style={style}>
                 <div className="room-map__room-name">#{ vnum(room.id) } { room.name }</div>
                 <div className="room-map__room-coordinates">{ room.coordinates.x }, { room.coordinates.y }</div>
+                <div className="room-map__room-exit">
+                    { exits.map(e => Exit(room, e)) }
+                </div>
             </div>
         );
+
+        //TODO: add room action on click hover arrow
+        //TODO: add "connect rooms" util function and auto-connect rooms when adding
+        //TODO: edit room popup on room click
+        //TODO: auto-show edit room for new room when clicking hover arrow
+        //TODO: size the room-map to fit all the rooms, make its container scroll: auto
+        //TODO: translate the whole room-map so negative indices are positive
+        //TODO: draw prettier exit and "add" arrow
     };
 
     const roomsMapped = mapper.generateCoordinates(roomsData.byId);
