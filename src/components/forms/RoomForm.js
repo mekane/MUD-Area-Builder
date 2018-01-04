ae2c48f38d8d250ed68d3cc84d50293e462b7cbb
Marty Kane <marty@pixotech.com>
2018-01-04 13:49:29 -0600

    Add inputs for the room numbers for the exits, conditionally shown

--- src/components/forms/RoomForm.js
+++ src/components/forms/RoomForm.js
@@ -9,7 +9,8 @@ class RoomForm extends React.Component {
         this.state = RoomForm.generateComponentState(props.room);
 
         this.handleRoomInputChange = this.handleRoomInputChange.bind(this);
-        this.handleExitInputChange = this.handleExitInputChange.bind(this);
+        this.handleExitToggleChange = this.handleExitToggleChange.bind(this);
+        this.handleExitRoomChange = this.handleExitRoomChange.bind(this);
     }
 
     static generateComponentState(room) {
@@ -24,6 +25,11 @@ class RoomForm extends React.Component {
         };
     }
 
+    roomDestination(direction) {
+        const exit = this.state.room.exit[direction];
+        return exit && exit.destination || 0;
+    }
+
     componentWillReceiveProps(nextProps) {
         if (nextProps && nextProps.room && nextProps.room !== this.state.room) {
             this.setState(RoomForm.generateComponentState(nextProps.room));
@@ -33,6 +39,8 @@ class RoomForm extends React.Component {
     updateInfo(that) {
         return function (e) {
             let room = that.state.room;
+
+            //todo: delete exits that are not checked
 
             if (room.id)
                 app.store.dispatch(app.actions.setRoomInfo(room));
@@ -44,30 +52,46 @@ class RoomForm extends React.Component {
     handleRoomInputChange(event) {
         const target = event.target;
         //const value = target.type === 'checkbox' ? target.checked : target.value;
-        const value = target.value;
-        const name = target.name;
+        const newInputValue = target.value;
+        const roomProperty = target.name;
 
-        const newRoomState = Object.assign({}, this.state.room, {[name]: value});
+        const newRoomState = Object.assign({}, this.state.room, {[roomProperty]: newInputValue});
 
         this.setState({room: newRoomState});
     }
 
-    handleExitInputChange(event) {
+    handleExitToggleChange(event) {
         const target = event.target;
-        const value = target.checked;
-        const name = target.name;
+        const hasExitBoolean = target.checked;
+        const exitDirection = target.name;
 
-        const newHasExitState = Object.assign({}, this.state.hasExit, {[name]: value});
+        const newHasExitState = Object.assign({}, this.state.hasExit, {[exitDirection]: hasExitBoolean});
 
         this.setState({hasExit: newHasExitState});
+    }
+
+    handleExitRoomChange(event) {
+        const target = event.target;
+        const destination = target.value;
+        const direction = target.name;
+
+        const exit = this.state.room.exit[direction];
+
+        const newExits = Object.assign({}, this.state.room.exit, {[direction]: {destination}});
+        const newRoomState = Object.assign({}, this.state.room, {exit: newExits});
+
+        this.setState({room: newRoomState});
     }
 
     render() {
         if (!this.props.room)
             return null; //don't render the room form when there is no active room
 
+        const visibleExitRoomFieldClass = 'room-form__room-input-label';
+        const hiddenExitRoomFieldClass = 'room-form__room-input-label--hidden';
+
         return <form className="room-form">
-            <span>Room {this.state.room.id}</span>
+            <span>Exiting Room {this.state.room.id}</span>
             <label className="room-form__label">
                 Room Name:
                 <input className="room-form__room-name" type="text" name="name" value={this.state.room.name}
@@ -76,34 +100,49 @@ class RoomForm extends React.Component {
 
             <label className="room-form__label">
                 Description:
-                <input className="room-form__room-description" type="text" name="description"
-                       value={this.state.room.description}
-                       onChange={this.handleRoomInputChange}/>
+                <input className="room-form__room-description"
+                       type="text" value={this.state.room.description} name="description" onChange={this.handleRoomInputChange}/>
             </label>
 
             <header>Exits</header>
             <label className="room-form__checkbox-label">
-                <input type="checkbox" checked={this.state.hasExit['n']} name="n"
-                       onChange={this.handleExitInputChange}/>
+                <input type="checkbox" checked={this.state.hasExit['n']} name="n" onChange={this.handleExitToggleChange}/>
                 North
             </label>
 
+            <label className={this.state.hasExit['n'] ? visibleExitRoomFieldClass : hiddenExitRoomFieldClass}>
+                North exit goes to room
+                <input className="room-form__exit-destination-input" type="number" value={this.roomDestination('n')} name="n" onChange={this.handleExitRoomChange}/>
+            </label>
+
             <label className="room-form__checkbox-label">
-                <input type="checkbox" checked={this.state.hasExit['e']} name="e"
-                       onChange={this.handleExitInputChange}/>
+                <input type="checkbox" checked={this.state.hasExit['e']} name="e" onChange={this.handleExitToggleChange}/>
                 East
             </label>
 
-            <label className="room-form__checkbox-label">
-                <input type="checkbox" checked={this.state.hasExit['s']} name="s"
-                       onChange={this.handleExitInputChange}/>
-                South
+            <label className={this.state.hasExit['e'] ? visibleExitRoomFieldClass : hiddenExitRoomFieldClass}>
+                East exit goes to room
+                <input className="room-form__exit-destination-input" type="number" value={this.roomDestination('e')} name="e" onChange={this.handleExitRoomChange}/>
             </label>
 
             <label className="room-form__checkbox-label">
-                <input type="checkbox" checked={this.state.hasExit['w']} name="w"
-                       onChange={this.handleExitInputChange}/>
+                <input type="checkbox" checked={this.state.hasExit['s']} name="s" onChange={this.handleExitToggleChange}/>
+                South
+            </label>
+
+            <label className={this.state.hasExit['s'] ? visibleExitRoomFieldClass : hiddenExitRoomFieldClass}>
+                South exit goes to room
+                <input className="room-form__exit-destination-input" type="number" value={this.roomDestination('s')} name="s" onChange={this.handleExitRoomChange}/>
+            </label>
+
+            <label className="room-form__checkbox-label">
+                <input type="checkbox" checked={this.state.hasExit['w']} name="w" onChange={this.handleExitToggleChange}/>
                 West
+            </label>
+
+            <label className={this.state.hasExit['w'] ? visibleExitRoomFieldClass : hiddenExitRoomFieldClass}>
+                West exit goes to room
+                <input className="room-form__exit-destination-input" type="number" value={this.roomDestination('w')} name="w" onChange={this.handleExitRoomChange}/>
             </label>
 
             <button type="button" className="room-form__update" onClick={this.updateInfo(this)}>Update</button>
